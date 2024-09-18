15fa85bf5ef2f6271f2e15366a39a6a5557f9f9f
saisamyuktha.g <saisamyuktha.g@thoughtworks.com>
2024-09-18 08:39:53 +0530

    Include startup script in dockerfile to set permissions for the documents

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -9,6 +9,7 @@ envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > ${OPENMRS_
 envsubst < /etc/bahmni-emr/templates/openmrs-runtime.properties.template > ${OPENMRS_APPLICATION_DATA_DIRECTORY}/openmrs-runtime.properties
 envsubst < /etc/bahmni-emr/templates/mail-config.properties.template > ${OPENMRS_APPLICATION_DATA_DIRECTORY}/mail-config.properties
 envsubst < /etc/bahmni-emr/templates/appointment.properties.template > ${OPENMRS_APPLICATION_DATA_DIRECTORY}/appointment.properties
+envsubst < /etc/bahmni-emr/templates/rulesengine-concept.properties.template > ${OPENMRS_APPLICATION_DATA_DIRECTORY}/rulesengine-concept.properties
 /openmrs/wait-for-it.sh --timeout=3600 ${OMRS_DB_HOSTNAME}:3306
 
 echo "Copy Configuration Folder from bahmni_config"
@@ -18,12 +19,14 @@ then
 fi
 mysql --host="${OMRS_DB_HOSTNAME}" --user="${OMRS_DB_USERNAME}" --password="${OMRS_DB_PASSWORD}" "${OMRS_DB_NAME}" -e "UPDATE global_property SET global_property.property_value = '' WHERE  global_property.property = 'search.indexVersion';" || true
 
+./update_elis_host_port.sh
+
 if [ "${OMRS_DOCKER_ENV}" = 'true' ]
 then
 echo "setting the folder permissions"
-setfacl -d -m o::rx -m g::rx /home/bahmni/document_images/
-setfacl -d -m o::rx -m g::rx /home/bahmni/uploaded_results/
-setfacl -d -m o::rx -m g::rx /home/bahmni/uploaded-files/
+setfacl -R -d -m o::rx -m g::rx /home/bahmni/document_images
+setfacl -R -d -m o::rx -m g::rx /home/bahmni/uploaded_results
+setfacl -R -d -m o::rx -m g::rx /home/bahmni/uploaded-files
 fi
 
 echo "Running OpenMRS Startup Script..."
